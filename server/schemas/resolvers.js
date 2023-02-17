1eac9020186f4cc8c3f8ef689d30aa92a8e03dce
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-02-17 10:45:13 -0700

    added resolvers info

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,40 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    user: async (parent, { username }) => {
+      return User.findOne({
+        // TODO: not sure this is accurate.
+        $or: [
+          { _id: user ? user._id : params.id },
+          { username: params.username },
+        ],
+      }).populate('books');
+    },
+  },
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('No user found with that email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
