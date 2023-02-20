02edc32f5d54685cb947cf249f92004e10bc4f5e
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-02-20 12:12:42 -0700

    added info in savedbooks, searchbooks, and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,54 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql``;
-export const ADD_USER = gql``;
-export const SAVE_BOOK = gql``;
-export const REMOVE_BOOK = gql``;
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const SAVE_BOOK = gql`
+  mutation saveBook($title: String!) {
+    book(title: $title) {
+      authors
+      description
+      bookId
+      image
+      link
+      title
+    }
+  }
+`;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($title: String!) {
+    book(title: $title) {
+      authors
+      description
+      bookId
+      image
+      link
+      title
+    }
+  }
+`;
+
+// make a search to google books api
+// https://www.googleapis.com/books/v1/volumes?q=harry+potter
+export const searchGoogleBooks = (query) => {
+  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+};
