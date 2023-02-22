55223e179c9e062c272641b5fbe7d818095a4fd9
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-02-22 16:12:57 -0700

    server works

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -27,7 +27,7 @@ const SavedBooks = () => {
             return false;
           }
 
-          const response = await getMe(token);
+          const response = await GET_ME(token);
 
           if (!response.ok) {
             throw new Error('something went wrong!');
